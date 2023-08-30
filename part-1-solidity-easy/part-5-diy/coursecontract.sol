b97eb82889ff473be3dbb9c49e3a0f487dcecca2
Akash Shaw <shawakash2003@gmail.com>
2023-08-30 21:10:57 +0530

    Ownable

--- part-1-solidity-easy/part-5-diy/coursecontract.sol
+++ part-1-solidity-easy/part-5-diy/coursecontract.sol
@@ -1,1 +1,20 @@
-// Try to write the contract here
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BasicContract {
+    // State variables
+    address public owner;
+    uint256 public value;
+
+    // Constructor
+    constructor() {
+        owner = msg.sender;
+        value = 0;
+    }
+
+    // Function to update the value
+    function updateValue(uint256 newValue) public {
+        require(msg.sender == owner, "Only the owner can update the value");
+        value = newValue;
+    }
+}
